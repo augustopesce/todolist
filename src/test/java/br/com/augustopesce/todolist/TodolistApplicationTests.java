ff649a36c85053ed9979611279d4a6fe63f9eb67
Augusto Pesce <guipesce@gmail.com>
2023-10-26 12:47:52 +0200

    TODOLIST

---
+++ src/test/java/br/com/augustopesce/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.augustopesce.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
